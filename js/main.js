5c36699d25e252134b587e8f7cf9ab88024d544e
Facu Cott <facumcott@gmail.com>
2022-06-30 10:02:43 -0300

    js:primera entrega

---
+++ js/main.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 4; i++) {
+
+    let ingresarNombre = prompt("Ingresar nombre");
+    alert(" Hola " + ingresarNombre + "!, te asignamos el turno N° " + i + ".");
+
+    if (i > 4) {
+        break;
+    }
+}
